27749a971836a1288e08bfa339f064be21b0dbc5
flomero <49640251+flomero@users.noreply.github.com>
2024-12-05 16:38:30 +0100

    ftp wip

--- srcs/requirements/ftpserver/tools/setup.sh
+++ srcs/requirements/ftpserver/tools/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! -f /run/secrets/ftp_password ]; then
+	echo "No secret found for ftp_password"
+	exit 1
+fi
+
+export FTP_PASS=$(cat /run/secrets/ftp_password)
+
 useradd -m $FTP_USER
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 chown $FTP_USER:$FTP_USER /home/$FTP_USER
